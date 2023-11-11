da03cbee423884e968c8607993571977159a402a
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-11-11 12:57:25 +0100

    DP_Strategy

---
+++ java/DP_Strategy/src/MallardDuck.java
@@ -0,0 +1,6 @@
+public class MallardDuck extends Duck{
+    public MallardDuck(FlyBehaviour flyBehaviour) {
+        super(flyBehaviour);
+    }
+    public void performFly(){ flyBehaviour.fly();}
+}
